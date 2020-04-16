18b637917378ed3b7a3ddd91d38ee66d4d6d76d6
DavGabrielian <49720192+DavGabrielian@users.noreply.github.com>
2020-04-16 14:35:20 +0400

    Add files via upload

---
+++ ChangeColorBackground.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('button')
+const body = document.querySelector('body')
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+
+body.style.backgroundColor = 'violet'
+button.addEventListener('click', changeBackground)
+
+function changeBackground(){
+const colorIndex= parseInt(Math.random()*colors.length)
+body.style.backgroundColor = colors[colorIndex]
+}
